29ec2f90eea57015407ef99a5e5f81a669cb2163
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-04-13 20:03:46 +0700

    Add Copilot Chat
    Update to Lua 5.4

--- lua/community.lua
+++ lua/community.lua
@@ -1,3 +1,4 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -39,6 +40,7 @@ return {
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
