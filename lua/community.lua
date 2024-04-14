7d74e61b7a5e2e8da08b8bed3a438b4170e883be
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-04-14 17:58:00 +0700

    Fix telescope with nightly

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -13,7 +12,9 @@ return {
     "toppair/peek.nvim",
     opts = { app = "google-chrome-stable", "--new-window" },
   },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   {
     "zbirenbaum/copilot.lua",
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  "telescope.nvim",
+  opts = {
+    defaults = {
+      sorting_strategy = "descending",
+      layout_config = {
+        horizontal = { prompt_position = "bottom", preview_width = 0.55 },
+        vertical = { mirror = false },
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
+    },
+  },
+}
