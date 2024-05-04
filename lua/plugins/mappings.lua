98c328c73818e4a8d13665922a93144cf855088c
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-05-04 22:23:34 +0700

    Add mappings to center cursor

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -18,6 +18,13 @@ return {
             end,
             desc = "Disconnect from Remote Neovim",
           },
+          -- Page up and down go to center of screen
+          ["<C-u>"] = { "<C-u>zz", desc = "Half page up" },
+          ["<C-d>"] = { "<C-d>zz", desc = "Half page down" },
+          ["n"] = { "nzzzv", desc = "Move to next search item" },
+          ["N"] = { "Nzzzv", desc = "Move to previous search item" },
+          ["C-f"] = { "C-fzz", desc = "Page down" },
+          ["C-b"] = { "C-bzz", desc = "Page up" },
         },
       },
     },
