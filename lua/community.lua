aef136e1048b2796c0f419d5345cf4fd60f2615b
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-05-21 22:42:40 +0700

    Remove nvim-spectre
    Replace flash with leap

--- lua/community.lua
+++ lua/community.lua
@@ -6,8 +6,6 @@
 return {
   { "AstroNvim/astrocommunity" },
   { import = "astrocommunity.color.nvim-highlight-colors" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.markdown-and-latex.peek-nvim" },
   {
     "toppair/peek.nvim",
@@ -38,7 +36,7 @@ return {
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
-  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.vim-matchup" },
@@ -48,6 +46,4 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.full-dadbod" },
-  { import = "astrocommunity.project.nvim-spectre" },
-  -- import/override with your plugins folder
 }
