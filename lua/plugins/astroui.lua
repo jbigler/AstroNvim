0e08ff1fe08e61938664268eb9018c67887e220d
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-05-31 12:08:54 +0700

    Specify mocha flavor of catppuccin theme

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -10,7 +10,7 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "catppuccin",
+    colorscheme = "catppuccin-mocha",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
