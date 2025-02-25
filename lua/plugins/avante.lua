7e3c3c1db7eee4c26dfd890fa3afd069b270d90d
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2025-02-25 21:11:47 +0700

    Use Claude 3.7

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,13 +2,10 @@ return {
   {
     "yetone/avante.nvim",
     opts = {
+      auto_suggestions_provider = "copilot",
+      provider = "copilot",
       copilot = {
-        endpoint = "https://api.githubcopilot.com/",
-        -- model = "o3-mini"
-        model = "claude-3.5-sonnet",
-        proxy = nil, -- [protocol://]host[:port] Use this proxy
-        allow_insecure = false, -- Allow insecure server connections
-        timeout = 30000, -- Timeout in milliseconds
+        model = "claude-3.7-sonnet",
         temperature = 0,
         max_tokens = 8192,
       },
