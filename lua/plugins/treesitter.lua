8b42eee50e0af3005bd2e54f421c96834168d96b
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2025-04-10 08:56:25 +0700

    Inject language into mise toml
    https://mise.jdx.dev/mise-cookbook/neovim.html
    The regex to match the file name at that link is wrong.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,30 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  init = function(plugin)
+    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    -- Luckily, the only things that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require "nvim-treesitter.query_predicates"
+    require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+      local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+      local filename = vim.fn.fnamemodify(filepath, ":t")
+      -- Match both ".mise.toml" and any filename containing "mise" followed by ".toml"
+      return filename == ".mise.toml" or string.match(filename, ".*mise.*%.toml$")
+    end, { force = true, all = false })
+  end,
   opts = {
     ensure_installed = {
+      "bash",
       "lua",
       "vim",
+      "toml",
       -- add more arguments for adding more treesitter parsers
     },
   },
