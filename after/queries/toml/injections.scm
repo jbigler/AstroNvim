ba04daa9cb2f0d1c2d9a548fb21b648772c86a55
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2025-07-22 08:27:05 +0700

    Update mise toml treesitter matcher

--- after/queries/toml/injections.scm
+++ after/queries/toml/injections.scm
@@ -5,7 +5,7 @@
   (string) @injection.content @injection.language
 
   (#is-mise?)
-  (#match? @injection.language "^\"\"\"\n*#!(/\\w+)+/env\\s+\\w+") ; multiline shebang using env
+  (#match? @injection.language "^['\"]{3}\n*#!(/\\w+)+/env\\s+\\w+") ; multiline shebang using env
   (#gsub! @injection.language "^.*#!/.*/env%s+([^%s]+).*" "%1") ; extract lang
   (#offset! @injection.content 0 3 0 -3) ; rm quotes
 )
@@ -15,7 +15,7 @@
   (string) @injection.content @injection.language
 
   (#is-mise?)
-  (#match? @injection.language "^\"\"\"\n*#!(/\\w+)+\s*\n") ; multiline shebang
+  (#match? @injection.language "^['\"]{3}\n*#!(/\\w+)+\s*\n") ; multiline shebang
   (#gsub! @injection.language "^.*#!/.*/([^/%s]+).*" "%1") ; extract lang
   (#offset! @injection.content 0 3 0 -3) ; rm quotes
 )
@@ -25,8 +25,8 @@
   (string) @injection.content
 
   (#is-mise?)
-  (#match? @injection.content "^\"\"\"\n*.*") ; multiline
-  (#not-match? @injection.content "^\"\"\"\n*#!") ; no shebang
+  (#match? @injection.content "^['\"]{3}\n*.*") ; multiline
+  (#not-match? @injection.content "^['\"]{3}\n*#!") ; no shebang
   (#offset! @injection.content 0 3 0 -3) ; rm quotes
   (#set! injection.language "bash") ; default to bash
 )
@@ -36,7 +36,7 @@
   (string) @injection.content
 
   (#is-mise?)
-  (#not-match? @injection.content "^\"\"\"") ; not multiline
+  (#not-match? @injection.content "^['\"]{3}") ; not multiline
   (#offset! @injection.content 0 1 0 -1) ; rm quotes
   (#set! injection.language "bash") ; default to bash
 )
