07afe8ae3ce551b2a87291f9e129a0b7875dfed0
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2026-08-31 21:23:51 +0700

    chore: limit lazy.nvim concurrency to 5

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
+  concurrency = 5,
   ui = { backdrop = 100 },
   performance = {
     rtp = {
